1ae47f419164cab221c26edc01ff87d800737109
G-Balamurugan <109854570+G-Balamurugan@users.noreply.github.com>
2024-01-19 14:42:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ from cron_translate import extractDetails
 import pandas as pd
 
 folderPath = '/Users/balamurugan/Desktop/RunDeck/Input_XML'
-outputFilePath = '/Users/balamurugan/Desktop/RunDeck/output_jobs.xlsx'
 inputJsonPath = '/Users/balamurugan/Desktop/RunDeck/Json_File/input_json.json'
+jsonFile = '/Users/balamurugan/Desktop/RunDeck/Json_File/final_json.json'
+jsonServiceFile = '/Users/balamurugan/Desktop/RunDeck/Json_File/final_service_json.json'
+outputFilePath = '/Users/balamurugan/Desktop/RunDeck/output_jobs.xlsx'
 finalJson = {}
 
 # Converting Input Xml into Json Format
@@ -41,9 +43,24 @@ for item in result:
 jsonObject = json.dumps(finalJson,indent=4)
 with open(inputJsonPath, "w") as outfile:
     outfile.write(jsonObject)
-finalJson = extractDetails(finalJson)
 
-df = pd.DataFrame.from_dict(finalJson, orient='index')
-df.to_excel(outputFilePath, index=False)
+resultJson = extractDetails(finalJson)
+finalJson , finalServiceJson = resultJson["finalJson"] , resultJson["finalServiceJson"]
+
+finalJsonObject = json.dumps(finalJson,indent=4)
+with open(jsonFile, "w") as outfile:
+    outfile.write(finalJsonObject)
+
+finalJsonObject = json.dumps(finalServiceJson,indent=4)
+with open(jsonServiceFile, "w") as outfile:
+    outfile.write(finalJsonObject)
+
+# To Include a sheet for All data too , in excel  
+finalServiceJson["All data"] = finalJson
+
+with pd.ExcelWriter(outputFilePath, engine='xlsxwriter') as writer:
+    for sheet_name, data in finalServiceJson.items():
+        df = pd.DataFrame.from_dict(data, orient='index')
+        df.to_excel(writer, sheet_name=sheet_name, index=False)
 
 print("Operation Completed .... Output is written in file --> " + outputFilePath )
